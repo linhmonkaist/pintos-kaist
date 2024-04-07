10c23bd8f4707704d34cae2cb026c990dad460fa
root <xavieraputri>
2024-04-07 06:59:18 +0000

    Viera -> syscall_fork,halt, exit, changes fork_aux structure

---
+++ userprog/build/userprog/syscall.d
@@ -0,0 +1,17 @@
+userprog/syscall.o: ../../userprog/syscall.c \
+ ../../include/userprog/syscall.h ../../include/lib/stdint.h \
+ ../../include/threads/thread.h ../../include/lib/debug.h \
+ ../../include/lib/kernel/list.h ../../include/lib/stdbool.h \
+ ../../include/lib/stddef.h ../../include/threads/synch.h \
+ ../../include/threads/interrupt.h ../../include/threads/fixed-point.h \
+ ../../include/lib/stdio.h ../../include/lib/stdarg.h \
+ ../../include/lib/kernel/stdio.h ../../include/lib/syscall-nr.h \
+ ../../include/threads/loader.h ../../include/threads/palloc.h \
+ ../../include/threads/malloc.h ../../include/threads/init.h \
+ ../../include/threads/mmu.h ../../include/threads/pte.h \
+ ../../include/threads/vaddr.h ../../include/filesys/file.h \
+ ../../include/filesys/off_t.h ../../include/filesys/filesys.h \
+ ../../include/userprog/gdt.h ../../include/userprog/process.h \
+ ../../include/threads/flags.h ../../include/intrinsic.h \
+ ../../include/threads/mmu.h ../../include/devices/input.h \
+ ../../include/lib/string.h
