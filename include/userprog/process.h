6404d550ece800343f873994256e373435a4921c
root <xavieraputri>
2024-05-25 05:15:49 +0000

    lab4_index_extensible_draft

--- include/userprog/process.h
+++ include/userprog/process.h
@@ -11,7 +11,9 @@ int process_exec (void *f_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (struct thread *next);
+#ifdef VM
 bool lazy_load_segment (struct page *page, void *aux);
+#endif
 
 struct fork_fd {
     struct thread *parent;
